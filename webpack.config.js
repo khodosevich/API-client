fc7a77dc2c700727e07b3837a6d5e84d3e4f38b2
Matvey <hodos.952795@gmail.com>
2023-06-28 16:27:15 +0300

    add production and development mode

--- webpack.config.js
+++ webpack.config.js
@@ -1,50 +1,70 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const TerserPlugin = require('terser-webpack-plugin');
 
-module.exports = {
-    mode: 'development',
-    entry: './src/index.js',
-    output: {
-        filename: 'bundle.js',
-        path: path.resolve(__dirname, 'dist')
-    },
-    devServer: {
-        static: {
-            directory: path.resolve(__dirname, 'dist')
+module.exports = (env,argv) => {
+
+    console.log("Application webpack mode is " , argv.mode);
+
+    return{
+        mode: argv.mode,
+        entry: './src/index.js',
+        output: {
+            filename: 'bundle.js',
+            path: path.resolve(__dirname, 'built')
         },
-        hot: true
-    },
-    module: {
-        rules: [
-            {
-                test: /\.(js|jsx)$/,
-                exclude: /node_modules/,
-                use: {
-                    loader: 'babel-loader',
-                    options: {
-                        presets: ['@babel/preset-env', '@babel/preset-react']
-                    }
+        devServer: {
+            static: {
+                directory: path.resolve(__dirname, 'built')
+            },
+            hot: true
+        },
+        module: {
+            rules: [
+                {
+                    test: /\.(js|jsx)$/,
+                    exclude: /node_modules/,
+                    use: {
+                        loader: 'babel-loader',
+                        options: {
+                            presets: ['@babel/preset-env', '@babel/preset-react']
+                        }
+                    },
+
                 },
+                {
+                    test: /\.html$/,
+                    use: ['html-loader'],
+                },
+                {
+                    test: /\.css$/,
+                    use: ['style-loader', 'css-loader'],
+                }
+            ]
+        },
+        resolve: {
+            extensions: ['.js', '.jsx']
+        },
 
-            },
-            {
-                test: /\.html$/,
-                use: ['html-loader'],
-            },
-            {
-                test: /\.css$/,
-                use: ['style-loader', 'css-loader'],
-            }
-        ]
-    },
-    resolve: {
-        extensions: ['.js', '.jsx']
-    },
+        plugins: [
+            new HtmlWebpackPlugin({
+                template: './public/index.html',
+                filename: 'index.html',
+            }),
+        ],
 
-    plugins: [
-        new HtmlWebpackPlugin({
-            template: './public/index.html',
-            filename: 'index.html',
-        }),
-    ],
+        optimization: argv.mode === "production" ? {
+            minimize: true,
+            minimizer:[
+                new TerserPlugin ({
+                    terserOptions: {
+                        output:{
+                            comments: false,
+                        },
+                    },
+                    extractComments:false
+                })
+            ]
+        } : {},
+    }
 };
